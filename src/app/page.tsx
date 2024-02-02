6e6b22c011b32bd08c9542ed32db13a1c238e1d2
Franco Ansaldi <106043508+francoansaldi7@users.noreply.github.com>
2024-02-01 22:19:56 -0300

    feat(slider): started working on the main slider, added swiper library, need to fix errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,33 @@
+"use client"
 import Header from '@/components/Header'
 import Image from 'next/image'
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 export default function Home() {
   return (
-    <main className='principal-banner flex min-h-screen flex-col items-center bg-neutral-13'>
+    <main className='principal-banner flex min-h-screen flex-col items-center bg-neutral-13 pt-40'>
+      <section>
+        <div>
+        <Swiper
+          modules={[Navigation, Pagination]}
+          spaceBetween={50}
+          slidesPerView={1}
+          navigation
+          pagination={{ clickable: true }}
+        >
+          <SwiperSlide><div className='bg-red'>Slide 1</div></SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+        </Swiper>
+        </div>
+      </section>
     </main>
   )
 }
